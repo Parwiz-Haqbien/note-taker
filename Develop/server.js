79ba9b9447184f2fc3de5074ffd10a5cefa0cf24
Parwiz Haqbien <parwiz.jan03@gmail.com>
2022-10-30 23:34:45 +1100

    finishing up

---
+++ Develop/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+const PORT = 3001;
+const app = express();
+app.use(express.urlencoded( {
+    extended: true
+}));
+app.use(express.json())
+
+app.use(express.static('public'));
+app.use(htmlRoutes);
+app.listen(PORT , () => {
+    console.log('listening on port' , PORT)
+});
+
+
+
